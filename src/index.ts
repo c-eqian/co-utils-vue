332095f50d5230a764575f7f034481f4c77ff3dd
C99127 <2429120006@qq.com>
2022-11-19 13:58:39 +0800

    ✨ feat: 打包

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@
  * @Author: 十三
  * @Date: 2022-09-24 11:55:25
  * @LastEditors: 十三
- * @LastEditTime: 2022-11-18 19:50:05
+ * @LastEditTime: 2022-11-19 13:35:00
  */
-import { cloneDeep } from '@/packages/clone-deep';
+// import { cloneDeep } from './packages/clone-deep';
+export * from './packages/clone-deep';
+export * from './packages/format-date';
 
-export { cloneDeep };
+// export { cloneDeep };
--- src/packages/format-date/index.ts
+++ src/packages/format-date/index.ts
@@ -4,7 +4,7 @@
  * @Author: 十三
  * @Date: 2022-11-18 11:17:57
  * @LastEditors: 十三
- * @LastEditTime: 2022-11-18 18:57:32
+ * @LastEditTime: 2022-11-19 13:46:17
  */
 /**
  * 时间日期格式化
@@ -17,13 +17,13 @@ export const formatDate = (date: number | string | Date, format = 'yyyy-MM-dd') 
   // 处理在IOS上对 2022-11-16 20:47:50格式会显示NAN的情况
   const _d = new Date(typeof date === 'string' ? date.replace(/-/, '/') : date);
   if (_d instanceof Date) {
-    const dateDict = {
-      yyyy: _d.getFullYear(),
-      M: _d.getMonth() + 1,
-      d: _d.getDate(),
-      H: _d.getHours(),
-      m: _d.getMinutes(),
-      s: _d.getSeconds(),
+    const dateDict: Record<string, string> = {
+      yyyy: `${_d.getFullYear()}`,
+      M: `${_d.getMonth() + 1}`,
+      d: `${_d.getDate()}`,
+      H: `${_d.getHours()}`,
+      m: `${_d.getMinutes()}`,
+      s: `${_d.getSeconds()}`,
       MM: `${_d.getMonth() + 101}`.substring(1),
       dd: `${_d.getDate() + 100}`.substring(1),
       HH: `${_d.getHours() + 100}`.substring(1),
