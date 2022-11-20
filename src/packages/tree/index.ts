eb42ed29ac2db57644dd0cf0cb453531076c92dc
C99127 <2429120006@qq.com>
2022-11-20 16:55:50 +0800

    🐞 fix: 修复树形结构bug

--- src/packages/tree/index.ts
+++ src/packages/tree/index.ts
@@ -4,29 +4,26 @@
  * @Author: 十三
  * @Date: 2022-11-20 12:26:10
  * @LastEditors: 十三
- * @LastEditTime: 2022-11-20 14:35:53
+ * @LastEditTime: 2022-11-20 16:53:50
  */
-export interface ITree {
-  // 父节点字段
-  parent: string;
-  // 标识值
-  value: null | number | string;
-  key: string;
-}
-export const arr2Tree = (
-  arrData: object[],
-  config: ITree = {
-    parent: 'parent',
-    value: null,
-    key: 'id'
-  }
-) => {
+
+/**
+ * 树形结构转换
+ * @param arrData 原数组数据
+ * @param parent 关联父节点的字段
+ * @param key 每条目唯一标识
+ * @param pid 作为父节点值
+ * @returns
+ */
+export const arr2Tree = (arrData: object[], parent = 'parent', key = 'id', pid = null) => {
   const res: object[] = [];
   arrData.forEach(item => {
-    if (item[config.parent] === config.value) {
+    if (item[parent] === pid) {
       const children = arr2Tree(
-        arrData.filter(v => v[config.parent] !== config.value),
-        (config.value = item[config.key])
+        arrData.filter(v => v[parent] !== pid),
+        parent,
+        key,
+        item[key]
       );
       children.length ? res.push({ ...item, children }) : res.push({ ...item });
     }
