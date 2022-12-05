edf063651d21f238f9cea2f46811dfce141b33fc
C99127 <2429120006@qq.com>
2022-12-05 14:52:31 +0800

    ✨ feat: 新增全屏hooks

--- src/packages/hooks/index.ts
+++ src/packages/hooks/index.ts
@@ -4,7 +4,8 @@
  * @Author: 十三
  * @Date: 2022-12-04 19:55:59
  * @LastEditors: 十三
- * @LastEditTime: 2022-12-04 20:07:59
+ * @LastEditTime: 2022-12-05 14:52:03
  */
 export * from './use-namespace';
 export * from './use-sfc-with-install';
+export * from './use-full-screen';
