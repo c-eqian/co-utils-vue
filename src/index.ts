a71473b40852b3654715a5da88e4d66945d7e2dc
C99127 <2429120006@qq.com>
2022-12-05 12:13:19 +0800

    ✨ feat: 新增导出方法

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@
  * @Author: 十三
  * @Date: 2022-09-24 11:55:25
  * @LastEditors: 十三
- * @LastEditTime: 2022-11-25 14:46:29
+ * @LastEditTime: 2022-12-05 12:12:50
  */
 export * from './packages/clone-deep';
 export * from './packages/format';
 export * from './packages/tree';
 export * from './packages/helper';
 export * from './packages/reg';
+export * from './packages/hooks';
