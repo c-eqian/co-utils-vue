6231c7ea266798ae8b8ffe64cbb44dd51faf2c45
C99127 <2429120006@qq.com>
2022-12-26 15:38:41 +0800

    ✨ feat: 新增treeToArr等

--- src/packages/tree/index.ts
+++ src/packages/tree/index.ts
@@ -4,8 +4,19 @@
  * @Author: 十三
  * @Date: 2022-11-20 12:26:10
  * @LastEditors: 十三
- * @LastEditTime: 2022-12-26 14:47:47
+ * @LastEditTime: 2022-12-26 15:18:43
  */
+import { Omit } from '@/packages/helper';
+import { cloneDeep } from '@/packages/clone-deep';
+
+export type IProps<T = any> = T & {
+  children: T[];
+};
+export interface IOptions {
+  parent: string;
+  key: string;
+  pid: string | number | null;
+}
 
 /**
  * 树形结构转换
@@ -15,16 +26,7 @@
  * @param pid 作为父节点值
  * @returns
  */
-export type IProps<T = any> = T & {
-  children: T[];
-};
-export interface IOptions {
-  parent: string;
-  key: string;
-  pid: string | number | null;
-}
-//
-export const arr2Tree = <T>(
+export const arrToTree = <T>(
   arrData: T[],
   options: IOptions = {
     parent: 'parent',
@@ -38,16 +40,36 @@ export const arr2Tree = <T>(
   options.pid = options.pid === undefined ? null : options.pid;
   arrData.forEach(item => {
     if (item[options.parent] === options.pid) {
-      const children = arr2Tree(
+      const children = arrToTree(
         arrData.filter(v => v[options.parent ?? 'parent'] !== options.pid),
         {
-          parent : options.parent,
+          parent: options.parent,
           key: options.key,
-          pid:item[options.key]
+          pid: item[options.key]
         }
       );
       children.length ? res.push({ ...item, children }) : res.push({ ...item, children: [] });
     }
   });
+  return res;
+};
+
+/**
+ * 树形结构扁平化
+ * @param data 
+ * @returns 
+ */
+export const treeToArr = <T>(data: T[]) => {
+  const res: Omit<T, 'children'>[] = [];
+  for (let i = 0; i < data.length; i++) {
+    const cloneData = cloneDeep(data[i]);
+    if (data[i]['children']?.length) {
+      delete cloneData['children'];
+      res.push(cloneData);
+      treeToArr(data[i]['children']);
+    } else {
+      res.push(cloneData);
+    }
+  }
   return res;
 };
