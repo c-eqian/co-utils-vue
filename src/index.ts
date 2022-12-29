bc779483376e17b9b4694eaf5bd461b4372af0d5
C99017 <2429120006@qq.com>
2022-12-29 17:30:35 +0800

    ✨ feat: 添加CoSocket

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@
  * @version:
  * @Author: 十三
  * @Date: 2022-09-24 11:55:25
- * @LastEditors: 十三
- * @LastEditTime: 2022-12-26 10:54:45
+ * @LastEditors: C99017 2429120006@qq.com
+ * @LastEditTime: 2022-12-29 17:29:38
  */
 export * from './packages/clone-deep';
 export * from './packages/format';
@@ -12,4 +12,5 @@ export * from './packages/tree';
 export * from './packages/helper';
 export * from './packages/is';
 export * from './packages/hooks';
-export * from './packages/get-age'
+export * from './packages/get-age';
+export * from './packages/socket';
