65e7cd1ead9e691fcd094d0b1a4c7ac49865d2ad
C99127 <2429120006@qq.com>
2023-01-18 18:51:32 +0800

    📃 docs: 更新日志

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -105,6 +105,10 @@ function customerNav() {
         
       // ]
     },
+    {
+      text: '更新日志',
+      link: '/examples/version/CHANGELOG'
+    },
     // {
     //   text: '后端',
     //   items: [
