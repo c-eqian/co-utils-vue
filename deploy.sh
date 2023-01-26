9b4475114c18ee0a611cd12c86b81c1aa51b914a
C99127 <2429120006@qq.com>
2023-01-26 12:53:05 +0800

    🐞 fix: 冲突解决

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,7 @@
 set -e
 
 # 构建
-<<<<<<< HEAD
-<<<<<<< HEAD
 # npm run build:types && rollup -c
-=======
-npm run build:types && rollup -c
->>>>>>> 🐳 chore: 添加自动化构建
-=======
-# npm run build:types && rollup -c
->>>>>>> 🐳 chore: 修改构建流程
-
 # 发布
 npm run release
 # 复制版本日志到文档日志
