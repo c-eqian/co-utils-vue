42421dfc6220b996dcfc0ad0f4c1a92106c9ec63
leisure <2429120006@qq.com.com>
2023-05-12 15:52:16 +0800

    feat: 优化useSort

--- src/packages/hooks/use-sort/index.ts
+++ src/packages/hooks/use-sort/index.ts
@@ -2,6 +2,7 @@ import { ref, Ref, watchEffect } from 'vue';
 
 export interface SortOption<T> {
   key?: keyof T;
+  sortOrder?: 'desc' | 'asc';
   compareFn?: (a: T, b: T) => number;
 }
 
@@ -10,7 +11,9 @@ export interface SortOption<T> {
  * 在useSort函数中，我们使用了watchEffect函数来监听data的变化。
  * 当data发生变化时，我们会根据options中的配置对数据进行排序，并将排序后的结果保存在sortedData中。
  * @param data data是一个Ref类型的数组，表示需要排序的数据；
- * @param options options是一个对象，用于配置排序选项。options对象中可以包含两个属性：key和compareFn。key表示需要根据哪个键值进行排序，compareFn表示自定义的比较函数。
+ * @param options options是一个对象，用于配置排序选项。options对象中可以包含三个属性：
+ * key、sortOrder和compareFn。key表示需要根据哪个键值进行排序，sortOrder表示升序还是降序，默认降序，
+ * compareFn表示自定义的比较函数。
  */
 export const useSort = <T>(data: Ref<T[]>, options: SortOption<T> = {}) => {
   const sortedData = ref<T[]>([] as T[]);
@@ -26,9 +29,11 @@ export const useSort = <T>(data: Ref<T[]>, options: SortOption<T> = {}) => {
         if (compareFn) {
           return compareFn(a, b);
         } else if (typeof aValue === 'number' && typeof bValue === 'number') {
-          return aValue - bValue;
+          return options.sortOrder === 'asc' ? aValue - bValue : bValue - aValue;
         } else {
-          return String(aValue).localeCompare(String(bValue));
+          return options.sortOrder === 'asc'
+            ? String(aValue).localeCompare(String(bValue))
+            : String(bValue).localeCompare(String(aValue));
         }
       });
     } else if (compareFn) {
