df084e6debd2246c0b2c7d53bd486f3a4e8b8bec
leisure <2429120006@qq.com.com>
2023-07-20 13:52:49 +0800

    feat: 添加isNumeric，优化useSortByKey

--- src/packages/is/index.ts
+++ src/packages/is/index.ts
@@ -45,6 +45,19 @@ export const isObjectLike = <T>(value: T) => {
 export const isNumber = <T>(value: T): boolean => {
   return typeof value === 'number' || (isObjectLike(value) && getTag(value) === '[object Number]');
 };
+
+/**
+ * 判断一个字符串是否是数字，包括小数点和其他符号
+ * @param value
+ * isNumeric('123') //true
+ * isNumeric('-45.67'); // 输出: true
+ * isNumeric('3.14e-10'); // 输出: true
+ * isNumeric('abc'); // 输出: false
+ */
+export const isNumeric = value => {
+  if (isNumber(value)) return isNumber(value);
+  return /^[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?$/.test(value);
+};
 export const isString = value => {
   const type = typeof value;
   return (
