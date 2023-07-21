0d1f1e655799c6782aa9d8adf748f4e8c6e28941
leisure <2429120006@qq.com.com>
2023-07-21 18:06:23 +0800

    feat: 新增useClearObject

---
+++ src/__test__/useLabelByVal.test.ts
@@ -0,0 +1,44 @@
+import { useLabelByVal } from '@/packages/hooks';
+describe('useLabelByVal', () => {
+  const testConfig1 = [
+    {
+      label: '苹果',
+      value: 'apple'
+    },
+    {
+      label: '香蕉',
+      value: 'banana'
+    },
+    {
+      label: '梨',
+      value: 'pear'
+    }
+  ];
+  const testConfig2 = [
+    {
+      label1: '苹果',
+      value1: 'apple'
+    },
+    {
+      label1: '香蕉',
+      value1: 'banana'
+    },
+    {
+      label1: '梨',
+      value1: 'pear'
+    }
+  ];
+  it('格式一', () => {
+    expect(useLabelByVal(testConfig1, 'banana')).toEqual('香蕉');
+    expect(useLabelByVal(testConfig1, 'glass')).toEqual('');
+  });
+  it('格式二', () => {
+    expect(
+      useLabelByVal(testConfig2, 'banana', {
+        valueKey: 'value1',
+        labelKey: 'label1'
+      })
+    ).toEqual('香蕉');
+    expect(useLabelByVal(testConfig2, 'glass')).toEqual('');
+  });
+});
