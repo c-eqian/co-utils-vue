413e503cf4e6f49b3a43ff395a2a8753136b6613
leisure <2429120006@qq.com.com>
2023-08-22 15:36:04 +0800

    test: 测试文件

--- __test__/getAge.test.ts
+++ __test__/getAge.test.ts
@@ -1,9 +1,9 @@
-import { getAge } from '../src/packages/get-age';
+import { getAge } from '../src/packages';
 
 describe('getAge', () => {
   it('should ', () => {
     expect(getAge('450616199905206666')).toBe(24);
-    expect(getAge('450616199908206666')).toBe(23);
+    expect(getAge('450616199908206666')).toBe(24);
     expect(getAge('450616299908206666')).toBe(-1);
     expect(getAge('450616199907266666')).toBe(24);
   });
